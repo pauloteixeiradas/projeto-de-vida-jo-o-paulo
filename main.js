11c33b36bd34e50adcabd67429238dfc9f239dec
pauloteixeiradas <163345612+pauloteixeiradas@users.noreply.github.com>
2024-04-26 15:29:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = Documentos.queryselectorAII(".botao");
+
+for(let i=0;i <botoes.length;i++);
+botoes[i].onclick=fungtion(){
+
+    for(let j=0;j=0;j<botoes.lemgth;j++){
+        botoes[j].classlist.remove("ativo")
+    }
+
+    botoes[i].classlist.remove("ativo");
